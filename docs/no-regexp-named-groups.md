39e7313a13c88efe27ee914042edf0f28daf0f93
Keith Cirkel <keithamus@users.noreply.github.com>
2020-04-29 15:26:53 +0100

    feat(no-regexp-named-groups): add no-regexp-named-groups

---
+++ docs/no-regexp-named-groups.md
@@ -0,0 +1,31 @@
+# no-regexp-named-groups
+
+This prevents the use of the RegExp named groups feature
+
+```js
+/(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/
+
+new RegExp('(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})')
+```
+
+These will not be allowed because they are not supported in the following browsers:
+
+ - Edge < 79
+ - Safari < 11.1
+ - Firefox (any version at the time of writing)
+ - Chrome < 64
+
+
+## What is the Fix?
+
+If readability is the main concern, using non-named groups with array-destructuring can be usable without hampering readability too much:
+
+```js
+// With named:
+const {year,month,day} = '2020-01-01'.match(/(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/).groups
+
+// Without named
+const [_, year, month, day] = '2020-01-01'.match(/(\d{4})-(\d{2})-(\d{2})/) || []
+```
+
+This can be safely disabled if you intend to compile code with the `@babel/plugin-transform-named-capturing-groups-regex` Babel plugin, or `@babel/preset-env`.
---
+++ test/no-regexp-named-group.js
@@ -0,0 +1,47 @@
+var rule = require('../lib/rules/no-regexp-named-group')
+var RuleTester = require('eslint').RuleTester
+
+var ruleTester = new RuleTester({parserOptions: {ecmaVersion: 2020}})
+
+ruleTester.run('no-regexp-named-group', rule, {
+  valid: [
+    {code: '/(?:a)/'},
+    {code: '/(?:a)/g'},
+    {code: 'RegExp("(?:a)b")'},
+    {code: 'RegExp("(?:a)b", "g")'},
+  ],
+  invalid: [
+    {
+      code: '/(?<name>)/',
+      errors: [
+        {
+          message: 'RegExp named groups are not supported in undefined'
+        }
+      ]
+    },
+    {
+      code: 'new RegExp("(?<name>)")',
+      errors: [
+        {
+          message: 'RegExp named groups are not supported in undefined'
+        }
+      ]
+    },
+    {
+      code: '/(?<$name>)/',
+      errors: [
+        {
+          message: 'RegExp named groups are not supported in undefined'
+        }
+      ]
+    },
+    {
+      code: '/(?<_name>)/',
+      errors: [
+        {
+          message: 'RegExp named groups are not supported in undefined'
+        }
+      ]
+    },
+  ]
+})
